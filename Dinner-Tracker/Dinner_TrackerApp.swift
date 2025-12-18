cab15e69fe2857311b5db6564f004f41314d456a
Andrew Laurin <alaurin10@gmail.com>
2025-12-18 15:24:56 -0800

    Initial Commit

---
+++ Dinner-Tracker/Dinner_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dinner_TrackerApp.swift
+//  Dinner-Tracker
+//
+//  Created by Andrew Laurin on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Dinner_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dinner-TrackerTests/Dinner_TrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Dinner_TrackerTests.swift
+//  Dinner-TrackerTests
+//
+//  Created by Andrew Laurin on 12/18/25.
+//
+
+import Testing
+
+struct Dinner_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
